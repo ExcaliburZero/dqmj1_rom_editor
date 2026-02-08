4ef6f30743a0e976aee8c45df95ade87faddbd6b
Christopher Wells <cwellsny@nycap.rr.com>
2026-02-08 10:50:35 -0800

    Write temp data to app directory

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,33 +1,46 @@
 use std::{
     fs::{self, File},
     io::Cursor,
-    path::Path,
+    path::PathBuf,
     time::Instant,
 };
 
 use binrw::{BinRead, BinWriterExt};
 use ds_rom::rom::{raw, Rom, RomLoadOptions};
+use tauri::Manager;
 
 use crate::dqmj1_rom::{btl_enmy_prm::BtlEnmyPrm, string_tables::StringTables};
 
+fn get_mod_directory(app: tauri::AppHandle, temp_directory: &str) -> PathBuf {
+    let app_directory = app.path().app_data_dir().unwrap();
+
+    fs::create_dir_all(&app_directory).unwrap();
+
+    app_directory.join("mods").join(temp_directory)
+}
+
 #[tauri::command]
-pub fn unpack_rom(rom_filepath: &str, temp_directory: &str) {
+pub fn unpack_rom(app: tauri::AppHandle, rom_filepath: &str, temp_directory: &str) {
+    let mod_directory = get_mod_directory(app, temp_directory);
+
     let now = Instant::now();
 
     let raw_rom = raw::Rom::from_file(rom_filepath).unwrap();
     let rom = Rom::extract(&raw_rom).unwrap();
-    rom.save(temp_directory, None).unwrap();
+    rom.save(mod_directory, None).unwrap();
 
     let elapsed = now.elapsed();
     println!("Unpacked ROM in: {elapsed:?}");
 }
 
 #[tauri::command]
-pub fn pack_rom(rom_filepath: &str, temp_directory: &str) {
-    println!("Writing patched ROM to: {rom_filepath:?}");
-    println!("Reading from temp dir: {temp_directory:?}");
+pub fn pack_rom(app: tauri::AppHandle, rom_filepath: &str, temp_directory: &str) {
+    let mod_directory = get_mod_directory(app, temp_directory);
 
-    let config_filepath = Path::new(temp_directory).join("config.yaml");
+    println!("Writing patched ROM to: {rom_filepath:?}");
+    println!("Reading from temp dir: {mod_directory:?}");
+
+    let config_filepath = mod_directory.join("config.yaml");
 
     let now = Instant::now();
 
@@ -40,10 +53,10 @@ pub fn pack_rom(rom_filepath: &str, temp_directory: &str) {
 }
 
 #[tauri::command]
-pub fn get_btl_enmy_prm(temp_directory: &str) -> BtlEnmyPrm {
-    let filepath = Path::new(temp_directory)
-        .join("files")
-        .join("BtlEnmyPrm.bin");
+pub fn get_btl_enmy_prm(app: tauri::AppHandle, temp_directory: &str) -> BtlEnmyPrm {
+    let mod_directory = get_mod_directory(app, temp_directory);
+
+    let filepath = mod_directory.join("files").join("BtlEnmyPrm.bin");
     println!("Reading BtlEnmyPrm from: {filepath:?}");
     let file_data = fs::read(filepath).unwrap();
 
@@ -51,10 +64,10 @@ pub fn get_btl_enmy_prm(temp_directory: &str) -> BtlEnmyPrm {
 }
 
 #[tauri::command]
-pub fn set_btl_enmy_prm(temp_directory: &str, btl_enmy_prm: BtlEnmyPrm) {
-    let filepath = Path::new(temp_directory)
-        .join("files")
-        .join("BtlEnmyPrm.bin");
+pub fn set_btl_enmy_prm(app: tauri::AppHandle, temp_directory: &str, btl_enmy_prm: BtlEnmyPrm) {
+    let mod_directory = get_mod_directory(app, temp_directory);
+
+    let filepath = mod_directory.join("files").join("BtlEnmyPrm.bin");
     println!("Writing BtlEnmyPrm to: {filepath:?}");
 
     let mut file = File::create(filepath).unwrap();
@@ -62,8 +75,10 @@ pub fn set_btl_enmy_prm(temp_directory: &str, btl_enmy_prm: BtlEnmyPrm) {
 }
 
 #[tauri::command]
-pub fn get_string_tables(temp_directory: &str) -> StringTables {
-    let filepath = Path::new(temp_directory).join("arm9").join("arm9.bin");
+pub fn get_string_tables(app: tauri::AppHandle, temp_directory: &str) -> StringTables {
+    let mod_directory = get_mod_directory(app, temp_directory);
+
+    let filepath = mod_directory.join("arm9").join("arm9.bin");
     println!("Reading string tables from ARM9 binary: {filepath:?}");
     let file_data = fs::read(filepath).unwrap();
 
