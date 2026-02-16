561b2a9024200e923d00868e3db980f61636b325
Christopher Wells <cwellsny@nycap.rr.com>
2026-02-16 09:09:01 -0800

    Remove commented out code

--- src-tauri/src/dqmj1_rom/regions.rs
+++ src-tauri/src/dqmj1_rom/regions.rs
@@ -9,11 +9,6 @@ pub enum Region {
 
 impl Region {
     pub fn from_game_code(game_code: &str) -> Option<Region> {
-        //let game_code_to_region = BTreeMap::from([
-        //    ([0x41, 0x4A, 0x52, 0x45], Region::NorthAmerica), // AJRE
-        //    ([0x41, 0x4A, 0x52, 0x4A], Region::Japan),        // AJRJ
-        //    ([0x41, 0x4A, 0x52, 0x50], Region::Europe),       // AJRP
-        //]);
         let game_code_to_region = BTreeMap::from([
             ("AJRE", Region::NorthAmerica),
             ("AJRJ", Region::Japan),
