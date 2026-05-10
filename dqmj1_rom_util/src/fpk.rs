f6db3c895fca899fa80503b3780e8969a11d5ca7
Christopher Wells <cwellsny@nycap.rr.com>
2026-05-09 22:02:48 -0700

    Implement fpk file reading

---
+++ dqmj1_rom_util/src/fpk.rs
@@ -0,0 +1,64 @@
+use binrw::{binread, io::SeekFrom};
+
+#[binread]
+#[brw(little)]
+#[derive(Debug, PartialEq)]
+pub struct FpkFile {
+    pub name_info: [u8; 0x20],
+    pub offset: u32,
+    pub size: u32,
+
+    #[br(
+        seek_before = SeekFrom::Start(offset as u64),
+        count = size,
+        restore_position
+    )]
+    pub data: Vec<u8>,
+}
+
+#[binread]
+#[brw(little)]
+#[derive(Debug, PartialEq)]
+#[br(magic = b"\x46\x50\x4B\x00")] // FPK
+pub struct Fpk {
+    pub num_files: u32,
+
+    #[br(count = num_files)]
+    pub files: Vec<FpkFile>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+
+    use binrw::BinRead;
+
+    use super::*;
+
+    fn read_fpk_from_file(filepath: &str) -> Fpk {
+        let mut reader = File::open(filepath).unwrap();
+        Fpk::read(&mut reader).unwrap()
+    }
+
+    #[test]
+    fn test_read_single_file_fpk() {
+        let actual = read_fpk_from_file("test/data/single_file.fpk");
+
+        let expected = Fpk {
+            num_files: 1,
+            files: vec![FpkFile {
+                name_info: [
+                    102, 105, 108, 101, 46, 116, 120, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+                offset: 48,
+                size: 16,
+                data: vec![
+                    104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 0, 0, 0, 0, 0,
+                ],
+            }],
+        };
+
+        assert_eq!(actual, expected);
+    }
+}
